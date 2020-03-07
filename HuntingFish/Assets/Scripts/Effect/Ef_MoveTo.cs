a40ca2167f7b0c0fcbf4f36e5f1225fdaa69a417
Ramsaykumo <a763492797@163.com>
2020-03-07 10:30:02 +0800

    commit the whole project

---
+++ HuntingFish/Assets/Scripts/Effect/Ef_MoveTo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ef_MoveTo : MonoBehaviour {
+
+    private GameObject goldCollect;
+    public float GoldMoveSpeed = 35f;
+	
+	void Start () {
+        goldCollect = GameObject.Find("GoldCollect");
+	}
+	
+	
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, goldCollect.transform.position, GoldMoveSpeed * Time.deltaTime);
+	}
+}
---
+++ HuntingFish/Assets/Scripts/MainSceneUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainSceneUI : MonoBehaviour {
+
+    public GameObject settingPanel;
+    public Toggle muteToogle;
+
+    void Start()
+    {
+        muteToogle.isOn = !AudioManger.Instance.IsMute;
+        
+    }
+
+    public void SwitchMute(bool isOn)
+    {
+        AudioManger.Instance.SwitchMuteState(isOn);
+    }
+
+    public void OnBackButtonDown()
+    {
+        PlayerPrefs.SetInt("gold", GameController.Instance.gold);
+        PlayerPrefs.SetInt("lv", GameController.Instance.lv);
+        PlayerPrefs.SetFloat("scd", GameController.Instance.smallTimer);
+        PlayerPrefs.SetFloat("bcd", GameController.Instance.bigTimer);
+        PlayerPrefs.SetInt("exp", GameController.Instance.exp);
+        int temp = (AudioManger.Instance.IsMute == false) ? 0 : 1;
+        PlayerPrefs.SetInt("mute", temp);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+
+    public void OnsSettingButtonDown()
+    {
+        settingPanel.SetActive(true);
+    }
+
+    public void OnCloseButtonDown()
+    {
+        settingPanel.SetActive(false);
+    }
+
+    
+
+}
